daaf9a7863dd74c3a823a2f42e2cf9313afaa4d3
basic <basic@E40>
2016-03-17 21:23:15 +0900

    개어렵

---
+++ Java_Project/src/sample_0317/Idontknow.java
@@ -0,0 +1,40 @@
+package sample_0317;
+
+public class Idontknow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1; i<=2; i++)
+			for(int j = 1; j<=3; j++){
+				System.out.print(i);
+				System.out.println(j);
+			}
+		
+	
+	
+	for(int b=3; b<=9; b+=2){
+		for(int a=3; a<=9; a+=2){
+			System.out.print(a);
+			System.out.print("*");
+			System.out.print(b);
+			System.out.print("=");
+			System.out.print(a*b);
+			System.out.print(" ");
+
+		}
+		System.out.println(" ");
+		
+	}
+	
+	for(int x=8; x>=1; x--){
+		for(int y=1; y<=9; y++){
+			System.out.print(x*1000+y);
+			System.out.print(" ");
+			}			
+		System.out.println(" ");
+	
+		}
+	}
+}
+
