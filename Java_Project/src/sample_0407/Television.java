00b15a3dd6ad035b1f09880b6048d2f02f6a5b63
basic <basic@E40>
2016-04-14 21:07:53 +0900

    This, Car, Television

--- Java_Project/src/sample_0407/Television.java
+++ Java_Project/src/sample_0407/Television.java
@@ -1,15 +1,26 @@
 package sample_0407;
 
 public class Television {
-	int channel;
-	int volume;
-	boolean onOff;
+	private int channel;
+	private int volume;
+	private boolean onOff;
 
+	Television(int c, int v, boolean o) {
+		this.channel = c;
+		this.volume = v;
+		this.onOff = o;
+	}
 	void pr() {
 		System.out.println(channel+" "+volume+" "+onOff);		
 	}
 	int changechannel(int ch){
 		channel = ch;
 		return channel;
+	}
+	public Television() {
+		this(10,20,false);
+		/*this.channel = 10;
+		this.volume = 10;
+		this.onOff = true;*/
 	}
 }
--- Java_Project/src/sample_0407/TelevisionTest.java
+++ Java_Project/src/sample_0407/TelevisionTest.java
@@ -4,21 +4,23 @@ public class TelevisionTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Television mytv= new Television();
-		mytv.channel = 18;
+		Television mytv= new Television(7,10,true);
+		/*mytv.channel = 18;
 		mytv.volume = 12;
-		mytv.onOff = true;
+		mytv.onOff = true;*/
 		
 		Television testtv = new Television();
-		testtv= mytv;
-		testtv.channel = 536;
+		//testtv= mytv;
+		//testtv.channel = 536;
+		
 		
 		mytv.pr();
+		testtv.pr();
 		
-		Television yourtv= new Television();
-		yourtv.channel = 18;
+		Television yourtv= new Television(11,20,true);
+		/*yourtv.channel = 18;
 		yourtv.volume = 12;
-		yourtv.onOff = true;
+		yourtv.onOff = true;*/
 		
 		System.out.println("cuurent channel :"+yourtv.changechannel(24));
 		
